039d250e50a69850b09ecd219df2ceebe435e49f
Ted Mielczarek <tmielczarek@fastly.com>
2023-05-25 17:21:56 -0400

    simple eval

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use base64::Engine;
+use captcha_a::{CaptchaBuilder, Font};
 use fastly::http::StatusCode;
 use fastly::{Error, Request, Response};
 use std::time::Instant;
@@ -6,8 +7,19 @@ use std::time::Instant;
 #[fastly::main]
 fn main(_req: Request) -> Result<Response, Error> {
     let start = Instant::now();
-    //TODO: generate an actual CAPTCHA
-    let image = include_bytes!("../see-no-evil.png");
+    let fonts = vec![Font::try_from_bytes(include_bytes!("../NotoSans-Regular.ttf")).unwrap()];
+    let builder = CaptchaBuilder {
+        //custom attribute
+        width: 120,
+        height: 40,
+        length: 4,
+        fonts: &fonts,
+        //default attribute
+        ..Default::default()
+    };
+    let captcha = builder.build().unwrap();
+    let image = &captcha.raw_data;
+    let answer = &captcha.phrase;
     let elapsed_ms = start.elapsed().as_millis();
     println!("Generated CAPTCHA in: {elapsed_ms}");
     let image_b64 = base64::engine::general_purpose::STANDARD_NO_PAD.encode(image);
@@ -20,6 +32,7 @@ fn main(_req: Request) -> Result<Response, Error> {
   </head>
   <body>
     <img src="data:image/png;base64,{image_b64}">
+    <p>Answer: {answer}</p>
     <p>Generated in {elapsed_ms} ms</p>
   </body>
 </html>
