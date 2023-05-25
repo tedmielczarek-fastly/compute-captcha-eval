9bc53e9c0053eae752ab2bbd79021341e1b04ad6
Ted Mielczarek <tmielczarek@fastly.com>
2023-05-25 15:06:34 -0400

    add impl using captcha crate

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use base64::Engine;
+use captcha::Difficulty;
 use fastly::http::StatusCode;
 use fastly::{Error, Request, Response};
 use std::time::Instant;
@@ -6,11 +7,12 @@ use std::time::Instant;
 #[fastly::main]
 fn main(_req: Request) -> Result<Response, Error> {
     let start = Instant::now();
-    //TODO: generate an actual CAPTCHA
-    let image = include_bytes!("../see-no-evil.png");
+    let (answer, image) = captcha::gen(Difficulty::Medium)
+        .as_tuple()
+        .expect("Failed to generate CAPTCHA");
     let elapsed_ms = start.elapsed().as_millis();
     println!("Generated CAPTCHA in: {elapsed_ms}");
-    let image_b64 = base64::engine::general_purpose::STANDARD_NO_PAD.encode(image);
+    let image_b64 = base64::engine::general_purpose::STANDARD_NO_PAD.encode(&image);
     let body = format!(
         r#"<!DOCTYPE html>
 <html>
@@ -20,6 +22,7 @@ fn main(_req: Request) -> Result<Response, Error> {
   </head>
   <body>
     <img src="data:image/png;base64,{image_b64}">
+    <p>Answer: {answer}</p>
     <p>Generated in {elapsed_ms} ms</p>
   </body>
 </html>
