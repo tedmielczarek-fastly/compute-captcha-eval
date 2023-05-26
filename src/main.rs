513d586a18e01bc43d8488b048db08a7e9432226
Ted Mielczarek <tmielczarek@fastly.com>
2023-05-26 10:21:13 -0400

    captcha-rs eval

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use base64::Engine;
+use captcha_rs::CaptchaBuilder;
 use fastly::http::StatusCode;
 use fastly::{Error, Request, Response};
 use std::time::Instant;
@@ -6,11 +7,18 @@ use std::time::Instant;
 #[fastly::main]
 fn main(_req: Request) -> Result<Response, Error> {
     let start = Instant::now();
-    //TODO: generate an actual CAPTCHA
-    let image = include_bytes!("../see-no-evil.png");
+    let captcha = CaptchaBuilder::new()
+        .length(5)
+        .width(220)
+        .height(120)
+        .dark_mode(false)
+        .complexity(5)
+        .compression(40)
+        .build();
+    let answer = &captcha.text;
     let elapsed_ms = start.elapsed().as_millis();
     println!("Generated CAPTCHA in: {elapsed_ms}");
-    let image_b64 = base64::engine::general_purpose::STANDARD_NO_PAD.encode(image);
+    let image_b64 = captcha.to_base64();
     let body = format!(
         r#"<!DOCTYPE html>
 <html>
@@ -19,7 +27,8 @@ fn main(_req: Request) -> Result<Response, Error> {
     <link rel="icon" href="data:,"></link>
   </head>
   <body>
-    <img src="data:image/png;base64,{image_b64}">
+    <img src="{image_b64}">
+    <p>Answer: {answer}</p>
     <p>Generated in {elapsed_ms} ms</p>
   </body>
 </html>
